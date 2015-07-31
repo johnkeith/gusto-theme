e3fbb70d18b4e9a33d2c496f7aa58dbaecde8db6
John Keith <johnkeith@outlook.com>
2015-07-30 23:59:17 -0400

    showing thumbnails by posts; white background, colored sidebar

--- functions.php
+++ functions.php
@@ -8,12 +8,10 @@ define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
 define( 'CHILD_THEME_VERSION', '2.1.2' );
 
 //* Enqueue Google Fonts
-add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
-function genesis_sample_google_fonts() {
-
-	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
-
-}
+// add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
+// function genesis_sample_google_fonts() {
+// 	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
+// }
 
 //* Add HTML5 markup structure
 add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
@@ -78,7 +76,7 @@ function genesis_do_custom_post_content(){
    // Only first post will display full content
     the_content();
   }else{
-    the_post_thumbnail('excerpt-thumbnail');
+    // the_post_thumbnail('excerpt-thumbnail');
     the_excerpt();
   }
 }
@@ -102,10 +100,10 @@ function custom_styles() {
 }
 
 function custom_google_fonts_styles() {
-	wp_enqueue_style( 'foodie-google-fonts', '//fonts.googleapis.com/css?family=Josefin+Sans:400,700|Open+Sans:300,400|Lato:300,400', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'foodie-google-fonts', '//fonts.googleapis.com/css?family=Oswald:300|Open+Sans:300,400|Lato:300,400', array(), CHILD_THEME_VERSION );
 }
 
-add_action( 'wp_enqueue_scripts', 'font_awesome_styles');
+add_action( 'wp_queryenqueue_scripts', 'font_awesome_styles');
 add_action( 'wp_enqueue_scripts', 'custom_styles');
 add_action( 'wp_enqueue_scripts', 'custom_google_fonts_styles');
 
@@ -152,3 +150,57 @@ if ( !is_page() ) {
   $post_info = '[post_date] [post_comments] [post_edit]';
   return $post_info;
 }}
+
+// customize read more link
+add_filter('excerpt_more', 'sp_read_more_link');
+add_filter('the_content_more_link', 'sp_read_more_link');
+function sp_read_more_link() {
+  return '...<a class="more-link" href="' . get_permalink() . '">Read More &rarr;</a>';
+}
+
+// add_action('genesis_before_entry', 'jk_show_post_thumbnail');
+// function jk_show_post_thumbnail(){
+//   global $wp_query;
+//   if($wp_query -> current_post == 0 && !is_paged()){ 
+//   }else{
+//     echo '<div class="entry-thumbnail-header-content">';
+//       the_post_thumbnail('excerpt-thumbnail');
+//   } 
+// }
+
+// add_action('genesis_after_entry', 'jk_close_entry_wrapper');
+// function jk_close_entry_wrapper(){
+//   global $wp_query;
+//   if($wp_query -> current_post == 0 && !is_paged()){ 
+//   }else{
+//     echo '</div>';
+//   } 
+// }
+// remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
+// remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+
+
+//* Move Post Title and Post Info from inside Entry Header to Entry Content on Posts page
+add_action( 'genesis_before_entry', 'reposition_entry_header' );
+function reposition_entry_header() {
+  if ( is_home() ) {
+    remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
+    remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+    remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+    remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+    
+    add_action( 'genesis_entry_content', 'jk_add_excerpt_thumb', 9);
+    add_action( 'genesis_entry_content', 'genesis_do_post_title', 9 );
+    add_action( 'genesis_entry_content', 'genesis_post_info', 9 );
+  }
+}
+
+function jk_add_excerpt_thumb() {
+  // TODO FIX FOR OTHER PAGES AFTER FISRT
+  // global $wp_query;
+  // if($wp_query -> current_post == 0 && !is_paged(){
+
+  // } else {
+    the_post_thumbnail('excerpt-thumbnail');    
+  // }
+}
