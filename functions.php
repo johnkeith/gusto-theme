5bf6a70c29697312f2663291f9215f532bfcb086
John Keith <johnkeith@outlook.com>
2015-08-02 18:26:57 -0400

    add permalink to all post thumbnails

--- functions.php
+++ functions.php
@@ -150,3 +150,10 @@ function reconfigure_post_content() {
 function jk_add_excerpt_thumb() {
   the_post_thumbnail('excerpt-thumbnail');    
 }
+
+
+add_filter( 'post_thumbnail_html', 'my_post_image_html', 10, 3 );
+function my_post_image_html( $html, $post_id, $post_image_id ) {
+  $html = '<a href="' . get_permalink( $post_id ) . '" title="' . esc_attr( get_post_field( 'post_title', $post_id ) ) . '">' . $html . '</a>';
+  return $html;
+}
